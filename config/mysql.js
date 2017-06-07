25ce005b6ece9394b4ac444bac984a2bce781b3e
wangbin <wangbin@hxkid.com>
2017-06-07 13:58:40 +0800

    调整随机时间

--- config/mysql.js
+++ config/mysql.js
@@ -1,12 +1,4 @@
 module.exports = {
-    /*"connInfo": {
-        host: 'chenxun.org',
-        port: 3306,
-        user: 'root',
-        pass: 'VJQIFQeMZIUG',
-        db: "chenxun",
-        charset: "utf8"
-    }*/
     "connInfo": {
         host: '192.168.3.6',
         port: 3306,
@@ -16,13 +8,3 @@ module.exports = {
         charset: "utf8"
     }
 };
-
-/*
-{
-    host: '47.93.229.22',
-        port: 3306,
-    user: 'root',
-    pass: 'VJQIFQeMZIUG',
-    db: "chenxun",
-    charset: "utf8"
-}*/
